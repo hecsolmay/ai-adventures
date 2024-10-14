8d7ff6f13338ba16ed785a9b4a68bd3e7ca854e6
hecsolmay <hecsolmay@gmail.com>
2024-10-14 13:08:09 -0600

    add custom hook useScrollAnimation

---
+++ src/app/hooks/useScrollAnimation.tsx
@@ -0,0 +1,36 @@
+import { useAnimation } from 'framer-motion'
+import { useState, useEffect } from 'react'
+
+export default function useScrollAnimation<T extends HTMLElement> () {
+  const controls = useAnimation()
+  const [ref, setRef] = useState<T | null>(null)
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        if (entry.isIntersecting) {
+          controls.start({ opacity: 1, y: 0 }).catch(error => {
+            console.error(error)
+          })
+        }
+      },
+      { threshold: 0.1 }
+    )
+
+    if (ref !== null) {
+      observer.observe(ref)
+    }
+
+    return () => {
+      if (ref !== null) {
+        observer.unobserve(ref)
+      }
+    }
+  }, [ref, controls])
+
+  return {
+    ref,
+    setRef,
+    controls
+  }
+}
