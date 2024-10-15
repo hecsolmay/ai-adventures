2560778b3468e5a9a5b5c901cd8925c6ab294dca
hecsolmay <hecsolmay@gmail.com>
2024-10-15 14:28:47 -0600

    add continueTaleSchema Validator

--- src/schemas/tales.ts
+++ src/schemas/tales.ts
@@ -2,6 +2,8 @@ import { z } from 'zod'
 
 import { genres } from '@/constants/ai'
 
+import { coreMessageSchema } from './messages'
+
 const lowerCaseGenres = genres.map(genre => genre.toLowerCase())
 
 export const beginTaleSchema = z.object({
@@ -17,10 +19,35 @@ export const beginTaleSchema = z.object({
     }),
 
   // THIS IS A TEXT REFERENCE USED TO IMPROVE THE TALE WITH THE USER CHOICE
-  textReference: z.string().trim().optional(),
+  imageURLBackground: z.string().trim().optional(),
 
   // THIS IS THE OPENAI API KEY USED TO GENERATE THE AI IF THE USER PROVIDES IT
   apiKey: z.string().optional()
 })
 
 export type BeginTale = z.infer<typeof beginTaleSchema>
+
+export const continueTaleSchema = z.object({
+  // VALIDATE MESSAGES
+  messages: z
+    .array(coreMessageSchema, {
+      message:
+        'Debes mandar un array de mensajes con el formato de CoreMessages (role = system o user o assistant, content = string)'
+    })
+    .min(
+      1,
+      'Al menos un elemento es requerido con el formato de CoreMessages (role = system o user o assistant, content = string)'
+    ),
+  choice: z
+    .string({ message: 'El texto de la opción elegida es requerida' })
+    .trim()
+    .min(1, 'El texto de la opción elegida es requerido'),
+
+  // THIS IS A TEXT REFERENCE USED TO IMPROVE THE TALE WITH THE USER CHOICE
+  imageURLBackground: z.string().trim().optional(),
+
+  // THIS IS THE OPENAI API KEY USED TO GENERATE THE AI IF THE USER PROVIDES IT
+  apiKey: z.string().optional()
+})
+
+export type ContinueTale = z.infer<typeof continueTaleSchema>
