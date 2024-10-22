cb2450c895fd63286f3fa3917cba6b0e4ccc5175
hecsolmay <hecsolmay@gmail.com>
2024-10-22 11:18:43 -0600

    add server props type

--- src/types/props.ts
+++ src/types/props.ts
@@ -3,3 +3,12 @@ export interface WithChildren {
   children?: React.ReactNode
 }
 export type SVGProps = React.SVGAttributes<SVGElement>
+
+export interface ServerParams {
+  id: string
+  slug: string
+}
+
+export interface ServerPageProps {
+  params: ServerParams
+}
