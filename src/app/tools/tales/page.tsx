433fd53221e1136a9144b56956b561fdf3fdd263
hecsolmay <hecsolmay@gmail.com>
2024-10-22 14:18:24 -0600

    add new Page on route /tools/tales

---
+++ src/app/tools/tales/page.tsx
@@ -0,0 +1,9 @@
+import StepTalesTransition from '@/components/tales/step-transition'
+
+export default function TalesPage () {
+  return (
+    <main className='mx-auto min-h-dvh max-w-7xl px-4 py-12 sm:px-6 lg:px-8'>
+      <StepTalesTransition />
+    </main>
+  )
+}
