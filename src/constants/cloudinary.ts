f0c0dd2065d73281ecbc78a05710a3e7a37afe23
hecsolmay <hecsolmay@gmail.com>
2024-11-12 11:39:46 -0600

    add constant cloudinary to list all public id's of characters

---
+++ src/constants/cloudinary.ts
@@ -0,0 +1,89 @@
+export const charactersPublicIds = [
+  {
+    publicId: 'ai-adventures/characters/brmzcmrrf3bdoc8ibajm'
+  },
+  {
+    publicId: 'ai-adventures/characters/pcpqc3jsjmdgiwxixhnq'
+  },
+  {
+    publicId: 'ai-adventures/characters/hjcoclfnzni7pcmxqb5g'
+  },
+  {
+    publicId: 'ai-adventures/characters/jtdqcm2n9z5tzrhkw9zb'
+  },
+  {
+    publicId: 'ai-adventures/characters/ysfhueked39lgy6sgs6k'
+  },
+  {
+    publicId: 'ai-adventures/characters/ub5d3lafbbuwaptkueyq'
+  },
+  {
+    publicId: 'ai-adventures/characters/skso910hd8knqdqcrob0'
+  },
+  {
+    publicId: 'ai-adventures/characters/dda0ct6q5z3qxocqkshi'
+  },
+  {
+    publicId: 'ai-adventures/characters/ms3gkoaeqyg9u39pqmm1'
+  },
+  {
+    publicId: 'ai-adventures/characters/cdhozmu4alsgee50yemj'
+  },
+  {
+    publicId: 'ai-adventures/characters/d3tu9wisaos79s75ufnu'
+  },
+  {
+    publicId: 'ai-adventures/characters/jsupzr84bj4dfvn8joa9'
+  },
+  {
+    publicId: 'ai-adventures/characters/igm9ssuldb9f61umfx6u'
+  },
+  {
+    publicId: 'ai-adventures/characters/t2gryxzjpiklldvko8ep'
+  },
+  {
+    publicId: 'ai-adventures/characters/wu50wtjuqmhccnhtvtng'
+  },
+  {
+    publicId: 'ai-adventures/characters/gkcca3jmss7ks2x3fjgj'
+  },
+  {
+    publicId: 'ai-adventures/characters/yl3w0wkmmgfzqykyrja2'
+  },
+  {
+    publicId: 'ai-adventures/characters/rv03yvppitj9ekcgldbr'
+  },
+  {
+    publicId: 'ai-adventures/characters/laoet8oyypglahstqnin'
+  },
+  {
+    publicId: 'ai-adventures/characters/eifo6ld67me3yvoahkeo'
+  },
+  {
+    publicId: 'ai-adventures/characters/f49ccpfafzvn2awiag9p'
+  },
+  {
+    publicId: 'ai-adventures/characters/y3rngzz23splmb9gup1v'
+  },
+  {
+    publicId: 'ai-adventures/characters/gwr212mgjsmjwoanqbpf'
+  },
+  {
+    publicId: 'ai-adventures/characters/uhkuygtkhze7zado4kvt'
+  },
+  {
+    publicId: 'ai-adventures/characters/vx5qgu7ytopy8mnmbgh5'
+  },
+  {
+    publicId: 'ai-adventures/characters/mt2bpvkvomijx5z7wyw8'
+  },
+  {
+    publicId: 'ai-adventures/characters/lq530w1uejqt9mmmpf5k'
+  },
+  {
+    publicId: 'ai-adventures/characters/aqwmmyjzdqeewcp4ffl4'
+  },
+  {
+    publicId: 'ai-adventures/characters/ts3xctqzr8gkb0s0lxg9'
+  }
+]
